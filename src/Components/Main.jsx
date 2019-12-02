4c4a180b4152bab6ce26f3391d21f32446919899
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-02 14:50:25 +0100

    +

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Container} from 'reactstrap';
-<<<<<<< HEAD
 import Navigation from './Navigation';
 import Profile from './Profile';
 
