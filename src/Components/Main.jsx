1bc6f31b9a7e97d483032b25fc3c4f38004530d6
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-02 14:48:30 +0100

    navbar +

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,11 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Container} from 'reactstrap';
 <<<<<<< HEAD
 import Navigation from './Navigation';
-import ProfileHeader from './ProfileHeader';
-=======
-import Navbar from './Navbar';
 import Profile from './Profile';
->>>>>>> 6bf8114287d50a264b5468d7fe532f5877bd4d0b
 
 class Main extends React.Component {
     render() {
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ProfileHeader from './ProfileHeader';
+import AboutUs from './AboutUs';
+import ProfileModal from './ProfileModal';
+import {Button} from 'reactstrap';
+
+class Profile extends React.Component {
+    state = {
+        profile: {},
+        modalOpen: false
+    }
+    render() {
+        return (
+            <div>
+                {this.state.profile? <>
+                <ProfileHeader profile={this.state.profile} /> 
+                <AboutUs aboutUs={this.state.profile.bio} /> </> : <div>Profile Loading...</div>}
+                <Button onClick={() => this.setState({modalOpen: true})}>Edit Profile</Button>
+                <ProfileModal profile={this.state.profile} open={this.state.modalOpen} close={this.state.modalOpen}/>
+            </div>
+           );
+    }
+    componentDidMount = async () => {
+        let username = "user16"
+        let password = "c9WEUxMS294hN6fF"
+        let token = btoa(username + ":" + password)
+        let response = await fetch("https://strive-school-testing-apis.herokuapp.com/api/profile/me", {
+            method: "GET",
+            headers: {
+                "Authorization": "Basic " + token
+      
+            }
+        })
+        let prof = await response.json()
+        console.log(prof);
+        this.setState({
+            profile: prof
+        })
+    };
+}
+
+export default Profile;
