3201c6bc360423fd4696b4c8647005055778242b
fainapahanko <fainapoganko@MacBook-Air-Faina.local>
2019-12-03 14:03:20 +0100

    search users done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Container} from 'reactstrap';
 import Navigation from './Navigation';
 import Profile from './Profile';
+import CurrentUserPage from './CurrentUserPage';
 
 class Main extends React.Component {
     render() {
@@ -11,6 +12,7 @@ class Main extends React.Component {
             <Navigation />
             <Container>
                 <Route path="/Profile" component={Profile} />
+                <Route path="/currentUserPage:userId" component={CurrentUserPage} />
             </Container>   
         </Router>
         );
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,83 +1,132 @@
 import React from 'react';
-import {Nav, NavItem,InputGroupAddon, Input, NavLink,InputGroupText, Navbar, NavbarBrand, InputGroup} from 'reactstrap';
+import {Nav, NavItem,InputGroupAddon, Input, NavLink,InputGroupText, Navbar, NavbarBrand, InputGroup, Dropdown, DropdownMenu, DropdownToggle } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faBriefcase, faSearch, faHome, faUsers, faComments, faBell, faTh } from '@fortawesome/free-solid-svg-icons'
 import '../index.css'
+import ProfilesDropDown from './ProfilesDropDown';
 
 
 
 class Navigation extends React.Component {
+   state = {
+      dropdownOpen: false,
+      setDropdownOpen: false,
+      srch: ''
+   }
+   toggleDropdown = (ev) => {
+      console.log(ev.target.value)
+      if(this.state.setDropdownOpen === false){
+         this.setState({
+            setDropdownOpen: true,
+            dropdownOpen: true,
+         })
+      }else if(this.state.setDropdownOpen === true){
+         this.setState({
+            setDropdownOpen: false,
+            dropdownOpen: false,
+         })
+      }
+   }
+   handleSearch = (ev) => {
+      this.setState({
+         srch: ev.target.value
+      }) 
+      if(ev.target.value.length === 0) {
+         this.setState({
+            setDropdownOpen: false,
+            dropdownOpen: false,
+         })
+      } else if(ev.target.value.length > 0){
+         this.setState({
+            setDropdownOpen: true,
+            dropdownOpen: true,
+         })
+      }
+   }
     render() {
-        return (
-           <Navbar className="nav-top">
+      return (
+         <Navbar className="nav-top">
             <Nav className="mr-auto"> 
-        <NavbarBrand href="/">
-               <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin}/>
-           </NavbarBrand>
-                <NavItem>
-                   <div className="search-div">
-                      <InputGroup className="search-input">
-                           <InputGroupAddon  addonType="prepend">
-                              <InputGroupText>                    <FontAwesomeIcon className="icon-search" icon={faSearch}/></InputGroupText>
-                           </InputGroupAddon>
-                           <Input className="main-input" placeholder="Search" />
+               <NavbarBrand href="/">
+                  <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin}/>
+               </NavbarBrand>
+               <NavItem>
+                  <div className="search-div">
+                     <InputGroup className="search-input">
+                        <InputGroupAddon  addonType="prepend">
+                           <InputGroupText>                    
+                              <FontAwesomeIcon className="icon-search" icon={faSearch}/>
+                           </InputGroupText>
+                        </InputGroupAddon>
+                        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                           <DropdownToggle
+                           tag="span"
+                           data-toggle="dropdown"
+                           aria-expanded={this.dropdownOpen}
+                           >
+                              <Input onChange={this.handleSearch} className="main-input" id="main-search-unput" placeholder="Search" />
+                           </DropdownToggle>
+                           <DropdownMenu>
+                              <ProfilesDropDown searchQuery={this.state.srch} toggleProfileDropdown={this.toggleDropdown}/>
+                           </DropdownMenu>
+                        </Dropdown>
                      </InputGroup>
-                   </div>
-                </NavItem>
-                 <NavItem>
-                    <div className="nav-item-div">
-                    <FontAwesomeIcon className="nav-icon" icon={faHome}/>
-                       <NavLink href="#">Home</NavLink>
-                    </div>
-                </NavItem>
-                <NavItem>
-                    <div className="nav-item-div">
-                    <FontAwesomeIcon className="nav-icon" icon={faUsers}/>
-                       <NavLink href="#" active>My Network</NavLink>
-                    </div>
-                 </NavItem>
-                 <NavItem>
-                    <div className="nav-item-div">
-                    <FontAwesomeIcon className="nav-icon" icon={faBriefcase}/>
-                       <NavLink href="#">Jobs</NavLink>
-                    </div>
-                 </NavItem>
-                 <NavItem>
-                    <div className="nav-item-div">
-                    <FontAwesomeIcon className="nav-icon" icon={faComments}/>
-                       <NavLink href="#">Messaging</NavLink>
                   </div>
-                 </NavItem>
-                 <NavItem>
-                    <div className="nav-item-div">
-                    <FontAwesomeIcon className="nav-icon" icon={faBell}/>
-                       <NavLink href="#">Notifications</NavLink>
-                    </div>
-                 </NavItem>
-                 <NavItem>
-                    <div className="nav-item-div">
-                       <div className="profile-image-div"></div>
-                    <NavLink href="#">Me</NavLink>
-                    </div>
-                 </NavItem>
-                 <div className="vl"></div>
-                 <NavItem>
-                     <div className="nav-item-div">
-                     <FontAwesomeIcon className="nav-icon" icon={faTh}/>
-                       <NavLink href="#">Work</NavLink>
-                     </div>
-                 </NavItem>
-                 <NavItem>
+               </NavItem>
+               <NavItem>
                   <div className="nav-item-div">
-                  <FontAwesomeIcon className="nav-icon" icon={faHome}/>
+                     <FontAwesomeIcon className="nav-icon" icon={faHome}/>
+                     <NavLink href="#">Home</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faUsers}/>
+                     <NavLink href="#" active>My Network</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faBriefcase}/>
+                     <NavLink href="#">Jobs</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faComments}/>
+                     <NavLink href="#">Messaging</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faBell}/>
+                     <NavLink href="#">Notifications</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <div className="profile-image-div"></div>
+                     <NavLink href="#">Me</NavLink>
+                  </div>
+               </NavItem>
+               <div className="vl"></div>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faTh}/>
+                     <NavLink href="#">Work</NavLink>
+                  </div>
+               </NavItem>
+               <NavItem>
+                  <div className="nav-item-div">
+                     <FontAwesomeIcon className="nav-icon" icon={faHome}/>
                      <NavLink href="#">Learning</NavLink>
                   </div>
-                  </NavItem>
-             </Nav>
-             </Navbar>
-        ); 
-    }
+               </NavItem>
+            </Nav>
+         </Navbar>
+      ); 
+   }
 }
 
 export default Navigation;
