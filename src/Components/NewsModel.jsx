abdfbc28316f94ef7f3713e18376307f0c195c2b
nehavenkat <neha.veketsetty@gmail.com>
2019-12-04 16:37:02 +0100

    created Toast/Modal and fetched info

---
+++ src/Components/NewsModel.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const NewsModel = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+
+const [modal, setModal] = useState(false);
+const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="primary" onClick={toggle}>{buttonLabel}</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle} color="blue">Enter Text</ModalHeader>
+        <ModalBody>
+        Your Text
+        {/*<input type="text" value={this.state.name} name="text" onChange={(data) => { this.setState({ text: data.target.value }) }} />*/}
+        </ModalBody>
+        <ModalFooter>
+        {/*<button onClick={() => { this.submit() }} > Submit Data </button>*/}
+          <Button color="primary" onClick={toggle}>Post</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default NewsModel;
